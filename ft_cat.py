ad804047957921875e9bc4b9fb5ac21426f8da89
Cyril Troian <cyril@Cyrils-MacBook-Air.local>
2018-08-22 18:47:15 +0300

    docstring added

--- ft_cat.py
+++ ft_cat.py
@@ -1,7 +1,13 @@
 from sys import argv
 
 def ft_cat(fn):
-	with open(argv[1], 'r') as fd:
+	'''
+Reproduction of the cat function, without extra options.
+'''
+	with open(fn, 'r') as fd:
 		print(fd.read(), end = '')
 
-ft_cat(argv[1])
+if len(argv) != 1:
+	argv.pop(0)
+	for file in argv:
+		ft_cat(file)
